bb86bf101f83a9478701a9ec06d822e97e7bb53c
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-03-18 13:44:11 -0400

    Update allowed hosts

--- shui/shui/settings.py
+++ shui/shui/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "*"  # TODO: Change
-]
+ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(',')
 
 
 # Application definition
